b6915efeb536b25b424c371221bfd6b74e035e95
Lukas Papay <papay.lukas@gmail.com>
2016-06-19 14:49:39 +0200

    Email configuration for development server

--- config/mail.php
+++ config/mail.php
@@ -80,7 +80,7 @@ return [
     |
     */
 
-    'username' => env('MAIL_USERNAME', 'baf93b3bc37f93'),
+    'username' => env('MAIL_USERNAME'),
 
     /*
     |--------------------------------------------------------------------------
@@ -93,7 +93,7 @@ return [
     |
     */
 
-    'password' => env('MAIL_PASSWORD', '2fc215577e1c92'),
+    'password' => env('MAIL_PASSWORD'),
 
     /*
     |--------------------------------------------------------------------------
