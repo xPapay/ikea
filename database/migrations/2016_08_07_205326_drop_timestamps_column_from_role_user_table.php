58df3f9735ec3d19fe306e2dc88ff88bfc75df06
Lukas Papay <papay.lukas@gmail.com>
2016-08-07 20:56:36 +0200

    Remove timestamps columns from role_user table

---
+++ database/migrations/2016_08_07_205326_drop_timestamps_column_from_role_user_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DropTimestampsColumnFromRoleUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('role_user', function (Blueprint $table) {
+            $table->dropColumn(['created_at', 'updated_at']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('role_user', function (Blueprint $table) {
+            //
+        });
+    }
+}
