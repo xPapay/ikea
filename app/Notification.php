b3f448b1544c5c67e120cc3b10525740b30c1e84
Lukas Papay <papay.lukas@gmail.com>
2016-08-07 19:28:49 +0200

    Remove soft delete from Notification

--- app/Notification.php
+++ app/Notification.php
@@ -4,12 +4,9 @@ namespace App;
 
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Notification extends Model
 {
-    use SoftDeletes;
-
     protected $fillable = [
         'type',
         'user_id',
