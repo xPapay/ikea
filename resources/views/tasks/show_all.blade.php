97165d2c2d2093d8c58cf192d8f47e6125b3f7d5
xPapay <papay.lukas@gmail.com>
2016-09-06 14:04:07 +0200

    Add color highlighting

--- resources/views/tasks/show_all.blade.php
+++ resources/views/tasks/show_all.blade.php
@@ -9,6 +9,9 @@
             </div>
         </div>
     {!! Form::close() !!}
+    <?php
+        $now = \Carbon\Carbon::now();
+    ?>
     <a href="{{ route('reset_filter') }}" class="btn btn-default">Resetovať filter</a>
 <table class="table table-hover">
     <thead>
@@ -24,7 +27,20 @@
     </thead>
     <tbody>
     @foreach($tasks as $task)
-        <tr>
+        @if($now->gt(\Carbon\Carbon::createFromFormat('d. m. Y', $task->task->deadline)))
+            <?php
+                $colour = 'danger';
+            ?>
+        @elseif($now->diffInDays(\Carbon\Carbon::createFromFormat('d. m. Y', $task->task->deadline)) <= 14)
+            <?php
+                $colour = 'warning';
+            ?>
+        @else
+            <?php
+                $colour = 'active';
+            ?>
+        @endif
+        <tr class="{{ $colour }}">
             <th></th>
             <td>
                 <a href="{{ action('TasksController@show', ['id' => $task->id, 'user_id' => $task->user_id]) }}">
